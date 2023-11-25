0c05b49f5668737ac6419304119d16a4db714fae
andreikestrel <andreibarbosa23@gmail.com>
2023-11-24 23:49:37 -0300

    Update API description and set page title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 function App() {
+  useEffect(() => {
+    // Define o título da página
+    document.title = 'DOCUMENTAÇÃO API DESAFIO 2 ESCRIBO';
+  }, []);
+
  return (
     <div className="App">
       <SwaggerUI url="/api-docs.json" />
